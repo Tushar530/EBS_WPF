2f22a0c04c36440858b9e21c5c3bdb6ee4ce0ac9
Tushar530 <tusharsoni95@gmail.com>
2018-10-05 13:14:58 +0530

    Add files via upload

---
+++ EBS_Exception/EBSException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBS_Exception
+{ /// <summary>
+  /// Author: Group3
+  /// Handles all the exceptions in EBS
+  /// </summary>
+    public class EBSException :ApplicationException
+    {
+        public EBSException() : base() { }
+
+
+
+        public EBSException(string errmessage) : base(errmessage) { }
+
+
+    }
+}
